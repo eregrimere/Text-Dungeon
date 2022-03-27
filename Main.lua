09cf7bb04a2bdb98ab16372eb9148977ddb9044a
Seikm <102381709+ProjetosAula@users.noreply.github.com>
2022-03-27 18:00:39 -0300

    table principal do pesonagem

---
+++ Main.lua
@@ -0,0 +1,14 @@
+local Personagem = {
+    ["Nome"] = nil,
+    ["Genero"] = nil,
+    ["Raca"] = nil,
+    ["Classe"] = nil,
+    ["Equipamentos"] = {},
+    ["Atributos"] = {
+        ["Forca"] = 0,
+        ["Destreza"] = 0,
+        ["Inteligencia"] = 0,
+        ["Resistencia"] = 0,
+        ["Carisma"] = 0
+    }
+}
