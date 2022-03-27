35b39083cc4aab2e60467dda80201ca13c18b6c7
Seikm <102381709+ProjetosAula@users.noreply.github.com>
2022-03-27 18:02:03 -0300

    Feita a distribuição de atributos

--- Main.lua
+++ Main.lua
@@ -16,11 +16,43 @@ local Personagem = {
 -- Criação de personagem
 
 print("Crie seu personagem:")
-io.write("Nome: ");
+io.write("Nome: ")
 Personagem["Nome"] = io.read("l")
-io.write("Genero: ");
+io.write("Genero: ")
 Personagem["Genero"] = io.read("l")
-io.write("Raca: ");
+io.write("Raca: ")
 Personagem["Raca"] = io.read("l")
-io.write("Classe: ");
+io.write("Classe: ")
 Personagem["Classe"] = io.read("l")
+
+-- Distribuição de pontos
+
+print("Distribua um total de 10 pontos para os atributos")
+
+function SomarTable(Table)
+    local SomaTotal = 0
+    for _, Valor in pairs(Table) do
+        SomaTotal = SomaTotal + Valor
+    end
+    return SomaTotal
+end
+
+function ConcatenarDic(Table)
+    local TextoFinal = ""
+    for Nome, Valor in pairs(Table) do
+        TextoFinal = TextoFinal .. "    " .. Nome .. ": " .. Valor .. "\n"
+    end
+    return TextoFinal
+end
+
+while SomarTable(Personagem["Atributos"]) ~= 10 do
+    io.write("Forca: ")
+    local ForcaColocar = io.read("n")
+    local Placeholder = io.read("l")
+    if SomarTable(Personagem["Atributos"]) + ForcaColocar > 10 then
+        print("O numero total de atributo deve ser igual a 10.")
+    else
+        Personagem["Atributos"]["Forca"] = Personagem["Atributos"]["Forca"] + ForcaColocar
+    end
+    print("Atributos totais:\n" .. ConcatenarDic(Personagem["Atributos"]))
+end
