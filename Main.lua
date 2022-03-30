56eec23b9dfbba0060be3aec971481ba302ea2ab
Seikm <102381709+ProjetosAula@users.noreply.github.com>
2022-03-30 13:19:50 -0300

    Nova table do player

---
+++ Main.lua
@@ -0,0 +1,12 @@
+local Player = {
+    Nome = nil,
+    Raca = nil,
+    Classe = nil,
+    Atributos = {
+        Forca = 0,
+        Destreza = 0,
+        Resistencia = 0
+    },
+    Equipamentos = {}
+}
+
