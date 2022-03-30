0faf7050cbe8ef19c39a8264c7f0e98de091f21b
Seikm <102381709+ProjetosAula@users.noreply.github.com>
2022-03-30 14:10:44 -0300

    Adicionado maneira para checar se o nome que o jogador colocou é valido

--- Main.lua
+++ Main.lua
@@ -13,7 +13,7 @@ local Player = {
 -- Protótipo de criação de personagem
 
 
--- Checa se uma string é valida
+-- Checa se uma string é uma palavra valida
 local function StringValida(String, QuantidadeLetras)
     if QuantidadeLetras == nil then
         QuantidadeLetras = 1 -- Se não for passado, assume que é 1
@@ -21,3 +21,13 @@ local function StringValida(String, QuantidadeLetras)
     if String == nil or #String < QuantidadeLetras or String:match("[%p%d]") then return false end
     return true -- retorna true se a string existe, não é vazia e não contém caracteres especiais
 end
+
+
+repeat -- Loop para checar se o nome é valido
+    io.write("Nome: ")
+    Player.Nome = io.read("l")
+    local NomeValido = StringValida(Player.Nome, 3)
+    if not NomeValido then
+        print("Nome invalido, coloque um nome com mais de 3 letras e sem caracteres especiais.")
+    end
+until NomeValido
