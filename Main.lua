caf96cb5499a1da258d8512c51c784e08dcf3f85
Seikm <102381709+ProjetosAula@users.noreply.github.com>
2022-03-30 15:27:04 -0300

    Implementado LuaFormatter

--- Main.lua
+++ Main.lua
@@ -2,57 +2,24 @@ local Player = {
     Nome = nil,
     Raca = nil,
     Classe = nil,
-    Atributos = {
-        Forca = 0,
-        Destreza = 0,
-        Resistencia = 0
-    },
+    Atributos = {Forca = 0, Destreza = 0, Resistencia = 0},
     Equipamentos = {}
 }
 
 local Racas = {
-    Humano = {
-        Forca = 2,
-        Destreza = 2,
-        Resistencia = 2
-    },
-    Elfo = {
-        Forca = 2,
-        Destreza = 3,
-        Resistencia = 1
-    },
-    Anao = {
-        Forca = 2,
-        Destreza = 1,
-        Resistencia = 3
-    },
-    Ogro = {
-        Forca = 3,
-        Destreza = 1,
-        Resistencia = 2
-    }
+    Humano = {Forca = 2, Destreza = 2, Resistencia = 2},
+    Elfo = {Forca = 2, Destreza = 3, Resistencia = 1},
+    Anao = {Forca = 2, Destreza = 1, Resistencia = 3},
+    Ogro = {Forca = 3, Destreza = 1, Resistencia = 2}
 }
 
 local Classes = {
-    Guerreiro = {
-        Forca = 2,
-        Destreza = 0,
-        Resistencia = 0
-    },
-    Ladino = {
-        Forca = 0,
-        Destreza = 2,
-        Resistencia = 0
-    },
-    Paladino = {
-        Forca = 0,
-        Destreza = 0,
-        Resistencia = 2
-    }
+    Guerreiro = {Forca = 2, Destreza = 0, Resistencia = 0},
+    Ladino = {Forca = 0, Destreza = 2, Resistencia = 0},
+    Paladino = {Forca = 0, Destreza = 0, Resistencia = 2}
 }
 
--- Protótipo de criação de personagem
-
+-- ? Protótipo de criação de personagem
 
 -- Checa se uma string é uma palavra valida
 local function StringValida(String, QuantidadeLetras)
@@ -66,20 +33,14 @@ end
 -- Lista os valores dentro de uma table
 local function ListarTable(Table)
     local TextoFinal = ""
-    for Nome, Valor in pairs(Table) do
-        TextoFinal = TextoFinal .. Nome .. "    "
-    end
+    for Nome, Valor in pairs(Table) do TextoFinal = TextoFinal .. Nome .. "    " end
     return TextoFinal
 end
 
 -- Juntar atributos de duas tables
 local function JuntarAtributos(Table)
-    for Nome, Valor in pairs(Table) do
-        Player.Atributos[Nome] = Player.Atributos[Nome] + Valor
-    end
+    for Nome, Valor in pairs(Table) do Player.Atributos[Nome] = Player.Atributos[Nome] + Valor end
 end
-
-
 
 repeat -- Loop para checar se o nome é valido
     io.write("Nome: ")
@@ -103,7 +64,8 @@ local function EscolherRacaClasse(Table, Table2, Tipo, Tipo2)
             end
         end
         if not Valido then
-            print("\n" .. Tipo .. " invalida, escolha uma das " .. Tipo2 .. " abaixo:\n" .. ListarTable(Table))
+            print("\n" .. Tipo .. " invalida, escolha uma das " .. Tipo2 .. " abaixo:\n" ..
+                      ListarTable(Table))
         end
     until Valido
 end
